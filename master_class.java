f034e1354ade42fa2f4d1d772d3439f10e928c95
MahadiAnkon <ankon1807075@stud.kuet.ac.bd>
2022-05-18 00:55:27 +0600

    Added Java Class

---
+++ master_class.java
@@ -0,0 +1,24 @@
+public class master_class{
+  int roll;
+  String name;
+
+  master_class(string a,int r)
+  {
+    this.roll=r;
+    this.name=a;
+  }
+  public int getroll()
+  {
+    return roll;
+  }
+  public void setroll(int roll){
+    this.roll=roll;
+  }
+  public void setname(String name){
+    this.name=name;
+  }
+  public String getname(){
+    
+    return name;
+}
+}
